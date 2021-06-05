01b834c2271c989810b997eb2a8fd13f748c8740
Veljko Vujcic <sambolino@gmail.com>
2021-06-05 21:56:42 +0300

    changed argument type to int

--- flarED-H_parser.py
+++ flarED-H_parser.py
@@ -8,7 +8,7 @@ from Range import Range
 PARSER = argparse.ArgumentParser(description="",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-PARSER.add_argument("-he", "--height", type=float, default=None, required=True,
+PARSER.add_argument("-he", "--height", type=int, default=None, required=True,
         choices=[Range(50, 90)], help="Altitudes [km]")
 ARGS = PARSER.parse_args()
 
