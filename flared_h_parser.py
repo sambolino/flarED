52954d8793fee8dda1bab917e51e545eacf1a9a9
Veljko Vujcic <sambolino@gmail.com>
2021-06-07 15:26:41 +0300

    renamed

--- flared_h_parser.py
+++ flared_h_parser.py
@@ -2,17 +2,16 @@
 
 import sys
 import argparse
-from flarED import flarED
+from flared import *
 from Range import Range
 
 PARSER = argparse.ArgumentParser(description="",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-PARSER.add_argument("-he", "--height", type=int, default=None, required=True,
-        choices=[Range(50, 90)], help="Altitudes [km]")
+PARSER.add_argument("-ix", "--ix", type=float, default=None, required=True,
+        choices=[Range(8.0e-07, 0.0001)], help="Solar X-Ray Flux")
 ARGS = PARSER.parse_args()
 
 if __name__ == "__main__":
-    f = flarED()
-    f.flared_t(ARGS.height)
-
+    f = flared_h(ARGS.ix)
+    f.plot()
